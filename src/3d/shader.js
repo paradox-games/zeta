64c4c4432498bf350ece24ba97c7f92999149c56
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-14 20:34:48 +0000

    Add LoadShader function to shader module (src/3d/shader.js)

--- src/3d/shader.js
+++ src/3d/shader.js
@@ -2,6 +2,22 @@
 // 3d objects shaders module
 // with shader class
 
+// shader loader function
+export const LoadShader = (gl, type, src, err_callback) => {
+	const shader = gl.createShader(type);
+	gl.shaderSource(shader, src);
+	gl.compileShader(shader);
+
+	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+		err_callback('An error occurred while compiling shader');
+		gl.deleteShader(shader);
+		return null;
+	}
+
+	return shader;
+}
+
+// shader class
 export const shader = class {
 	constructor(content, shtype, type) {
 		if (type.toLowerCase() == 'file') {
