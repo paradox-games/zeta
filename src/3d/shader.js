38fb566d18ad974189b05bff03d4e9e65e500dbe
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-14 20:28:59 +0000

    Add vertex & fragment shaders to shader module (src/3d/shader.js)

--- src/3d/shader.js
+++ src/3d/shader.js
@@ -3,13 +3,22 @@
 // with shader class
 
 export const shader = class {
-	constructor(content, type) {
+	constructor(content, shtype, type) {
 		if (type.toLowerCase() == 'file') {
 			fetch(content)
 			  .then(response => response.text())
-			  .then(text => this.content = text);
+			  .then(text => { 
+			  if (shtype.toLowerCase() == 'vertex') {
+			  	this.vertexsh = text;
+			  } else if (shtype.toLowerCase() == 'fragment') {
+			  	this.fragsh = text
+			  }});
 		} else if (type.toLowerCase() == 'code') {
-			this.content = content;
+			if (shtype.toLowerCase() == 'vertex') {
+				this.vertexsh = content;
+			} else if (shtype.toLowerCase() == 'fragment') {
+				this.fragsh = content;
+			}
 		}
 	}
 	// constructor
