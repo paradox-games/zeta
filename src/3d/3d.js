2d49ec36f86a930a9a2cb1fc1561d50f9df4b4b4
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-14 21:08:58 +0000

    Create 3d modules include file (src/3d/3d.js)

---
+++ src/3d/3d.js
@@ -0,0 +1,8 @@
+// ZETA ENGINE:
+// 3d modules include
+// file
+
+export * as object from './object'
+export * as scene  from './scene'
+export * as shader from './shader'
+export * as webgl  from './webgl'
--- src/zeta.js
+++ src/zeta.js
@@ -2,5 +2,6 @@
 // Engine main file, contains
 // 2d, 3d, Canvas & more stuff...
 
-export * as keyboard from './keyboard';
-export { canvas } from './canvas';
+export * as keyboard from './keyboard'
+export *             from './canvas'
+export * as z3d       from './3d/3d'
