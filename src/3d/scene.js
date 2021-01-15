2a441eddaf8d6aee349d4ffb028aaca390b7aa47
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-15 16:25:00 +0000

    Add matrices (projection & model view) to scene class (src/3d/scene.js)

--- src/3d/scene.js
+++ src/3d/scene.js
@@ -6,8 +6,13 @@ import { webgl_ctx } from './webgl'
 
 // scene class
 export const scene = class {
-	constructor(color, shader, clear, depth_func, depth_test=false) {
+	constructor(color=[0.0, 0.0, 0.0, 1.0], shader, clear=1.0, depth_func=webgl_ctx.LEQUAL, field_of_view, near, far, depth_test=false) {
 		this.shader = shader;
+		this.field_of_view = field_of_view;
+		this.projection_matrix = mat4.create();
+		this.model_view_matrix = mat4.create();
+		this.znear = near;
+		this.zfar = far;
 		webgl_ctx.clearColor(color[0], color[1], color[2], color[3]);
 		webgl_ctx.clearDepth(clear);
 		webgl_ctx.depthFunc(depth_func);
