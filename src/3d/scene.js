377ebef39c200b83b8f65c739261be9df8f86548
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-15 16:18:04 +0000

    Clear canvas before start drawing to it (src/3d/scene.js)

--- src/3d/scene.js
+++ src/3d/scene.js
@@ -2,19 +2,21 @@
 // 3d scenes module with
 // scene class
 
-import { shader } from './shader';
+import { webgl_ctx } from './webgl'
 
 // scene class
 export const scene = class {
 	constructor(color, shader, clear, depth_func, depth_test=false) {
 		this.shader = shader;
-		gl.clearColor(color[0], color[1], color[2], color[3]);
-		gl.clearDepth(clear);
-		gl.depthFunc(depth_func);
+		webgl_ctx.clearColor(color[0], color[1], color[2], color[3]);
+		webgl_ctx.clearDepth(clear);
+		webgl_ctx.depthFunc(depth_func);
 
 		if (depth_test) {
-			gl.enable(gl.DEPTH_TEST);
+			webgl_ctx.enable(gl.DEPTH_TEST);
 		}
+
+		webgl_ctx.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	}
 	// constructor
 }
