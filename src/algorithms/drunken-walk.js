7fea102e45074d82eade9005fd8481a47764b22f
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-16 21:22:00 +0000

    Add enemies to mazes generated using drunken walk algorithm (src/algorithms/drunken-walk.js)

--- src/algorithms/drunken-walk.js
+++ src/algorithms/drunken-walk.js
@@ -2,6 +2,9 @@
 // Drunken walk algorithm
 // implementation
 
+// X means player
+// O means enemy
+// . means floor
 export const maze = (width, height, iterations) => {
     let maze = [[]];
     for (let i = 0; i < height-1; i++) {
@@ -16,33 +19,40 @@ export const maze = (width, height, iterations) => {
     let playerX = Math.floor(Math.random() * Math.pow(10, width.toString().length)) % width;
     let playerY = Math.floor(Math.random() * Math.pow(10, height.toString().length)) % height;
 
-    let maze_write = () => {
+    let maze_write = (enemy) => {
         if (maze[playerY][playerX] == 'X')
             return;
         else
-            maze[playerY][playerX] == '.';
+            if (enemy)
+                maze[playerY][playerX] = 'O';
+            else
+                maze[playerY][playerX] = '.';
     }
 
     maze[playerY][playerX] = 'X'; //where the player starts
 
     for (let i = 0; i < iterations; i++) {
         let instruction = Math.random() * 10 % 4;
+        let enemy = Math.random() * 100;
+        if (enemy <= 15) enemy = true;
+        else enemy = false;
+    
         switch(instruction) {
             case 0: case 1:
                 playerX--;
-                maze_write();
+                maze_write(enemy);
                 break;
             case 2:
                 playerY++;
-                maze_write();
+                maze_write(enemy);
                 break;
             case 3:
                 playerX++;
-                maze_write();
+                maze_write(enemy);
                 break;
             case 4:
                 playerY--;
-                maze_write()
+                maze_write(enemy)
                 break;
         }
     }
