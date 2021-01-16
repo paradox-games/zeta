63ed17177a0621a250bd0ed8949bf9774ab1a3d6
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-16 14:02:46 +0000

    Add circle/arc draw function to Obj2d class (src/2d/object.js)

--- src/2d/object.js
+++ src/2d/object.js
@@ -3,9 +3,8 @@
 // Obj2d class
 
 export const Obj2d = class {
-    constructor(x, y, width, height, canvas, imgtype, color) {
-        this.width = width;
-        this.height = height;
+    constructor(x, y, properties, canvas, imgtype, color) {
+        this.properties = properties;
         this.x = x; this.y = y;
         this.canvas = canvas;
         this.ctx = this.canvas.element().getContext("2d");
@@ -18,7 +17,15 @@ export const Obj2d = class {
         switch (this.imgtype) {
         case 'rectangle':
             this.ctx.beginPath();
-            this.ctx.rect(this.x, this.y, this.width, this.height);
+            this.ctx.rect(this.x, this.y, this.properties["width"], this.properties["height"]);
+            this.ctx.fillStyle = this.color;
+            this.ctx.fill();
+            this.ctx.closePath();
+            break;
+        case 'circle':
+        case 'arc':
+            this.ctx.beginPath();
+            this.ctx.arc(this.x, this.y, this.properties["radius"], this.properties["start_angle"], this.properties["end_angle"]);
             this.ctx.fillStyle = this.color;
             this.ctx.fill();
             this.ctx.closePath();
