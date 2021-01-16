e8ca9acb71fdeeb7698f0db4db9a1e28966461e3
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-16 16:17:51 +0000

    Create zeta 2d include module (src/2d/2d.js)

---
+++ src/2d/2d.js
@@ -0,0 +1,6 @@
+// ZETA ENGINE:
+// 2d modules include
+// file
+
+export * as object from './object'
+export * as scene  from './scene'
--- src/zeta.js
+++ src/zeta.js
@@ -3,5 +3,7 @@
 // 2d, 3d, Canvas & more stuff...
 
 export * as keyboard from './keyboard'
-export *             from './canvas'
-export * as z3d       from './3d/3d'
+export { canvas }    from './canvas'
+
+export * as z3d from './3d/3d'
+export * as z2d from './2d/2d'
