a1f94860650d9b0cab3f4746610cd66c78407d8c
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-16 13:55:59 +0000

    Add constructor & draw function to Obj2d class (src/2d/object.js)

--- src/2d/object.js
+++ src/2d/object.js
@@ -2,4 +2,27 @@
 // 2d object module with
 // Obj2d class
 
-export const Obj2d = class {}
+export const Obj2d = class {
+    constructor(x, y, width, height, canvas, imgtype, color) {
+        this.width = width;
+        this.height = height;
+        this.x = x; this.y = y;
+        this.canvas = canvas;
+        this.ctx = this.canvas.element().getContext("2d");
+        this.imgtype = imgtype;
+        this.color = color;
+    }
+    // constructor
+
+    draw() {
+        switch (this.imgtype) {
+        case 'rectangle':
+            this.ctx.beginPath();
+            this.ctx.rect(this.x, this.y, this.width, this.height);
+            this.ctx.fillStyle = this.color;
+            this.ctx.fill();
+            this.ctx.closePath();
+            break;
+        }
+    }
+}
