25089924f5312f898d90d485731e372b7d7af4fd
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-17 13:49:00 +0000

    Add "id <canvas-id>" instruction to drawscript (src/2d/drawscript/drawscript.js)

--- src/2d/drawscript/drawscript.js
+++ src/2d/drawscript/drawscript.js
@@ -2,6 +2,22 @@
 // Canvas drawing scripting
 // language main module
 
-export const DrawScript = () => {
-    return;
+export const DrawScript = (script) => {
+    let splitted = script.split(' ');
+    let canvas = null;
+    let ctx = null;
+    let flags = {
+        'id': false
+    };
+
+    splitted.forEach(word => {
+        if (flags.id) {
+            canvas = document.getElementById(word);
+            ctx = canvas.getContext('2d');
+        }
+        switch (word) {
+        case 'id':
+            flags.id = true;
+        } 
+    });
 }
