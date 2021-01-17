ac1e9498d22b893453e5e0b20424a2110ac6fba4
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-17 19:51:07 +0000

    Add "rect <x> <y> <width> <height>" instruction to drawscript (src/2d/drawscript.js)

--- src/2d/drawscript.js
+++ src/2d/drawscript.js
@@ -7,17 +7,47 @@ export const DrawScript = (script) => {
     let canvas = null;
     let ctx = null;
     let flags = {
-        'id': false
+        'id': false,
+        'rect': 0
     };
+
+    let shapes = {
+        'rect': [0, 0, 0, 0]
+    }
 
     splitted.forEach(word => {
         if (flags.id) {
             canvas = document.getElementById(word);
             ctx = canvas.getContext('2d');
+            flags.id = false;
+        } else if (flags.rect) {
+            switch (flags.rect) {
+            case 4:
+                shapes.rect[0] = Number(word);
+                flags.rect--;
+                break;
+            case 3:
+                shapes.rect[1] = Number(word);
+                flags.rect--;
+                break;
+            case 2:
+                shapes.rect[2] = Number(word);
+                flags.rect--;
+                break;
+            case 1:
+                shapes.rect[3] = Number(word);
+                ctx.beginPath();
+                ctx.rect(shapes.rect[0], shapes.rect[1], shapes.rect[2], shapes.rect[3]);
+                ctx.stroke();
+                flags.rect--;
+                break;
+            }
         }
         switch (word) {
         case 'id':
             flags.id = true;
+        case 'rect':
+            flags.rect = 4;
         } 
     });
 }
