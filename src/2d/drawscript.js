31ddbd82a42bc2d5ff2552b514db09226ae5a1a2
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-26 20:50:06 +0000

    Add arc instruction to drawscript (src/2d/drawscript.js)

--- src/2d/drawscript.js
+++ src/2d/drawscript.js
@@ -8,11 +8,13 @@ export const DrawScript = (script) => {
     let ctx = null;
     let flags = {
         'id': false,
-        'rect': 0
+        'rect': 0,
+        'arc': 0
     };
 
     let shapes = {
-        'rect': [0, 0, 0, 0]
+        'rect': [0, 0, 0, 0],
+        'arc': [0, 0, 0, 0, 0]
     }
 
     splitted.forEach(word => {
@@ -42,8 +44,32 @@ export const DrawScript = (script) => {
                 flags.rect--;
                 break;
             }
-        }
-        switch (word) {
+        } else if (flags.arc) {
+            switch (flags.arc) {
+            case 5:
+                shapes.rect[0] = Number(word);
+                flags.rect--;
+                break;
+            case 4:
+                shapes.rect[1] = Number(word);
+                flags.rect--;
+                break;
+            case 3:
+                shapes.rect[2] = Number(word);
+                flags.rect--;
+                break;
+            case 2:
+                shapes.arc[3] = Number(word);
+                flags.arc--;
+            case 1:
+                shapes.rect[4] = Number(word);
+                ctx.beginPath();
+                ctx.arc(shapes.arc[0], shapes.arc[1], shapes.arc[2], shapes.arc[3], shapes.arc[4], false);
+                ctx.stroke();
+                flags.rect--;
+                break;
+            }
+        } else switch (word) {
         case 'id':
             flags.id = true;
         case 'rect':
